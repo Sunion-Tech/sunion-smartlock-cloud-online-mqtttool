5cb32a9b8321c5f0c05b5084740584fdf4a931ca
Mason0615 <mason@sunion.com.tw>
2020-06-24 09:31:29 +0800

    change endpoint

--- src/aws-iot-configuration.js
+++ src/aws-iot-configuration.js
@@ -1,7 +1,7 @@
 var awsIotConfiguration = {
-    endpoint: 'wss://a2mvse6841elo7-ats.iot.us-east-1.amazonaws.com/mqtt', 
+    endpoint: 'wss://a156heaxd3jfk-ats.iot.us-east-1.amazonaws.com/mqtt', 
     region: 'us-east-1',
-    poolId: 'us-east-1:de620ece-f07e-4041-803a-f0a2028129ea',
-    host: 'a2mvse6841elo7-ats.iot.us-east-1.amazonaws.com'
+    poolId: 'us-east-1_fyRCoumgr',
+    host: 'a156heaxd3jfk-ats.iot.us-east-1.amazonaws.com'
  };
  module.exports = awsIotConfiguration;

fce10da208563bc40c865d9db2a1f3902f5520da
Mason0615 <mason_chen@sunion.com.tw>
2021-12-03 16:39:52 +0800

    update to 443 port

--- src/aws-iot-configuration.js
+++ src/aws-iot-configuration.js
@@ -1,5 +1,5 @@
 var awsIotConfiguration = {
-    endpoint: 'wss://a3jcdl3hkiliu4-ats.iot.us-east-1.amazonaws.com/mqtt', 
+    endpoint: 'wss://a3jcdl3hkiliu4-ats.iot.us-east-1.amazonaws.com:443/mqtt', 
     region: 'us-east-1',
     poolId: 'us-east-1_9H0qG2JDz',
     host: 'a3jcdl3hkiliu4-ats.iot.us-east-1.amazonaws.com'

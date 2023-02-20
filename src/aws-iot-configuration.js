a96b421e3d9a06f5fdf6448cbb0857d7dd62284a
MasonChen <mason_chen@sunion.com.tw>
2023-02-20 17:30:42 +0800

    update pool id

--- src/aws-iot-configuration.js
+++ src/aws-iot-configuration.js
@@ -1,7 +1,7 @@
 var awsIotConfiguration = {
     endpoint: 'wss://a3jcdl3hkiliu4-ats.iot.us-west-1.amazonaws.com/mqtt', 
     region: 'us-west-1',
-    poolId: 'us-west-1_9H0qG2JDz',
+    poolId: 'us-west-1_bmX5gEvhg',
     host: 'a3jcdl3hkiliu4-ats.iot.us-west-1.amazonaws.com'
  };
  module.exports = awsIotConfiguration;

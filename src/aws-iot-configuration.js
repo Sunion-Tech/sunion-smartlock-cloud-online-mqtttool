74f88715dc261f774145cb2483069bff22c3c27e
MasonChen <mason_chen@sunion.com.tw>
2023-02-20 15:51:20 +0800

    change config

--- src/aws-iot-configuration.js
+++ src/aws-iot-configuration.js
@@ -1,7 +1,7 @@
 var awsIotConfiguration = {
-    endpoint: 'wss://a3jcdl3hkiliu4-ats.iot.us-east-1.amazonaws.com/mqtt', 
-    region: 'us-east-1',
-    poolId: 'us-east-1_9H0qG2JDz',
-    host: 'a3jcdl3hkiliu4-ats.iot.us-east-1.amazonaws.com'
+    endpoint: 'wss://a3jcdl3hkiliu4-ats.iot.us-west-1.amazonaws.com/mqtt', 
+    region: 'us-west-1',
+    poolId: 'us-west-1_9H0qG2JDz',
+    host: 'a3jcdl3hkiliu4-ats.iot.us-west-1.amazonaws.com'
  };
  module.exports = awsIotConfiguration;
